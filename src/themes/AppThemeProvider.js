db685000d5e3dd72d181935720f8cab444399173
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-11 01:02:02 +0530

    creating customize theme

--- src/themes/AppThemeProvider.js
+++ src/themes/AppThemeProvider.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import {createTheme,ThemeProvider} from '@mui/material/styles'
 
-const AppThemeProvider = () => {
+const theme = createTheme({
+    typography:{
+        allVariants:{
+            fontFamily:'Raleway',
+            textTransform:'none',
+            fontSize:15,
+        }
+    },
+    palette:{
+        primary:{
+            main:grey[700]
+        },
+        secondary:{
+            main:pink[200]
+        }
+    },
+    components:{
+        MuiTypography:{
+            defaultProps:{
+                sx:{
+                    px:1
+                },
+                variant:'subtitle2',
+                textTransform:'capitalize'
+            }
+        },
+        MuiStack:{
+            defaultProps:{
+                sx:{
+                    px:2,
+                    py:2
+                },
+                spacing:2,
+                direction:'row'
+            }
+        }
+    }
+});
+
+const AppThemeProvider = (prop) => {
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider theme={theme}>
+{prop.childern}
+    </ThemeProvider>
   )
 }
 
