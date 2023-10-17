9d81daf739d13c034eae7fa2325f45ef375a5330
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-18 00:15:07 +0530

    added header in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Box, CssBaseline } from "@mui/material";
+import Header from "components/Header.js";
+import React from "react";
+
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <Box>
+          <Header />
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 }
 
 export default App;
