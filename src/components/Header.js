8ccd1c372e6201d5c304421b1e93c0beacc5c0de
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-17 23:39:37 +0530

    working on header file

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import { Box, Container } from '@mui/material';
+import React from 'react';
+import { flexBetweenCenter,dFlex,displayOnDesktop } from "themes/CommonTheme.js";
+
+import Logo from './Logo';
+
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
