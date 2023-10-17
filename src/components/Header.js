02137de6b24495105d836ed5f271c494a59de953
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-17 23:46:26 +0530

    completed header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
-import { Box, Container } from '@mui/material';
-import React from 'react';
-import { flexBetweenCenter,dFlex,displayOnDesktop } from "themes/CommonTheme.js";
+import { Box, Container } from "@mui/material";
+import React from "react";
+import { flexBetweenCenter, dFlex } from "themes/CommonTheme.js";
 
-import Logo from './Logo';
-
+import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box
+      sx={{
+        ...dFlex,
+        minHeight: 70,
+        borderBottom: "1px solid #ddd",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            ...flexBetweenCenter,
+            minHeight: 70,
+            px: 4,
+          }}
+        >
+          <Logo />
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
