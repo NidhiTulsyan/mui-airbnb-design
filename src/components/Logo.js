b3bf27c538272db0dc67d44b7ab988979d8d18ca
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-17 23:21:52 +0530

    working on  logo file

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,11 @@
+import { Box, Typography } from '@mui/material'
+import {FaAirbnb} from 'react-icons/fa';
 import React from 'react'
 
 function Logo() {
   return (
     <div>
-      logo
+      
     </div>
   )
 }
