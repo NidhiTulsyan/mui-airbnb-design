e7ca23dacf51b9dd010a30aaaa9d4b65d8f92369
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-17 23:36:35 +0530

    logo file completed

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,26 @@
 import { Box, Typography } from "@mui/material";
 import { FaAirbnb } from "react-icons/fa";
 import React from "react";
-import { flexCenter } from "../themes/CommonTheme.js";
+import { flexCenter } from "themes/CommonTheme.js";
 import { pink } from "@mui/material/colors";
 
 function Logo() {
-  return <div></div>;
+  return (
+    <Box sx={flexCenter}>
+      <FaAirbnb size={40} color={pink[500]} />
+      <Typography
+        sx={{
+          ml: 1,
+          fontWeight: "bold",
+          fontSize: "40px",
+          color: (theme) => theme.palette.secondary.main,
+        }}
+        component={"h3"}
+      >
+        airBnB
+      </Typography>
+    </Box>
+  );
 }
 
 export default Logo;
