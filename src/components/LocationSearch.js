ebc9293f7d1942729d3fe2b377153cead6159f8a
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-18 23:56:26 +0530

    working doing loactionSerach file

--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -1,7 +1,15 @@
+import { Box, Button, Divider, Paper, Stack, Typography } from "@mui/material";
+import { pink } from "@mui/material/colors";
+import { IoSearchCircleSharp } from "react-icons/io5";
 import React from "react";
+const choices = [
+  { id: 1, text: "Anywhere" },
+  { id: 2, text: "Any week" },
+  { id: 3, text: "Any Guest", withIcon: true },
+];
 
 const LocationSearch = () => {
-  return <div></div>;
+  return <Box></Box>;
 };
 
 export default LocationSearch;
