45fae652bdf79106f6fbba4d658581771e1d9d82
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-20 15:16:36 +0530

    started profilesetting.js file

--- src/components/ProfileSettings.js
+++ src/components/ProfileSettings.js
@@ -1,4 +1,5 @@
 import { Box, Button, Link, Stack } from "@mui/material";
+import { flexCenter} from "themes/CommonTheme.js";
 import { BsGlobe } from "react-icons/bs";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaRegUserCircle } from "react-icons/fa";
