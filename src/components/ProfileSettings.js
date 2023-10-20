f644dbed2f56cc8781fb3bd4d0623fac46ad5a3c
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-20 15:21:12 +0530

    completed profileStting file

--- src/components/ProfileSettings.js
+++ src/components/ProfileSettings.js
@@ -1,5 +1,5 @@
 import { Box, Button, Link, Stack } from "@mui/material";
-import { flexCenter} from "themes/CommonTheme.js";
+import { flexCenter } from "themes/CommonTheme.js";
 import { BsGlobe } from "react-icons/bs";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaRegUserCircle } from "react-icons/fa";
@@ -7,7 +7,23 @@ import { FaRegUserCircle } from "react-icons/fa";
 import React from "react";
 
 const ProfileSettings = () => {
-  return <div></div>;
+  return (
+    <Box sx={flexCenter}>
+      <Link href="#">Become A host</Link>
+      <Stack>
+        <Button><BsGlobe size={24}/></Button>
+        <Button sx={{
+            borderRadius:10,
+            border:'1px solid #ddd'
+        }}>
+            <Stack>
+                <AiOutlineMenu size={24} />
+                <FaRegUserCircle size={24} />
+            </Stack>
+        </Button>
+      </Stack>
+    </Box>
+  );
 };
 
 export default ProfileSettings;
