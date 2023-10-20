c62b51addd3c3a2325b0a6d6fe73753e6399d0a4
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-20 15:15:55 +0530

    started profilesetting.js file

--- src/components/ProfileSettings.js
+++ src/components/ProfileSettings.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Box, Button, Link, Stack } from "@mui/material";
+import { BsGlobe } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import React from "react";
 
 const ProfileSettings = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default ProfileSettings
+export default ProfileSettings;
