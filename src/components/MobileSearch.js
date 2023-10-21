509022fe7682543dd7e929113f623afbd795f8ca
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-21 23:54:08 +0530

    started mobileSearch file

--- src/components/MobileSearch.js
+++ src/components/MobileSearch.js
@@ -4,11 +4,18 @@ import { FaSearch } from "react-icons/fa";
 import { VscSettings } from "react-icons/vsc";
 
 const MobileSearch = () => {
-  return <Paper component="form" sx={{
-    p:'2px 4px',
-    display:'flex',
-    
-  }}></Paper>;
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        border: "1px solid #ccc",
+      }}
+    ></Paper>
+  );
 };
 
 export default MobileSearch;
