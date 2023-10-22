05089dff00e56b1d8ead4d863e1629569e15bf73
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-22 21:49:11 +0530

    complered mobile search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { Box, Container } from "@mui/material";
 import React from "react";
-import { flexBetweenCenter, dFlex , displayOnDesktop } from "themes/CommonTheme.js";
+import {
+  flexBetweenCenter,
+  dFlex,
+  displayOnDesktop,
+} from "themes/CommonTheme.js";
 
 import Logo from "./Logo";
 import LocationSearch from "./LocationSearch";
@@ -24,13 +28,19 @@ const Header = () => {
             px: 4,
           }}
         >
-          
-        <Box sx={displayOnDesktop}><Logo /></Box>
-        <Box sx={displayOnDesktop}><LocationSearch /></Box>
-        <Box sx={displayOnDesktop}>  <ProfileSettings /></Box>
-          
-        
-          <MobileSearch />
+          <Box sx={displayOnDesktop}>
+            <Logo />
+          </Box>
+          <Box sx={displayOnDesktop}>
+            <LocationSearch />
+          </Box>
+          <Box sx={displayOnDesktop}>
+            {" "}
+            <ProfileSettings />
+          </Box>
+          <Box sx={{ display: { xs: "flex" , md:'none'} }}>
+            <MobileSearch />
+          </Box>
         </Box>
       </Container>
     </Box>
