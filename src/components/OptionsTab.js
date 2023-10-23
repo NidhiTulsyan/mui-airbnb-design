f453661be1add82488c5623c97b52871c32efe4b
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-23 23:16:56 +0530

    working on option tab file

---
+++ src/components/OptionsTab.js
@@ -0,0 +1,10 @@
+import { Box, Button, Container, Tab, Tabs } from "@mui/material";
+import { FaFilter } from "react-icons/fa";
+import React from "react";
+import { locationsTab } from "data/Mock-data";
+
+const OptionsTab = () => {
+  return <Box></Box>;
+};
+
+export default OptionsTab;
