00f0dc0a406f9e8764f86fcb782d32ad1ee7d4a1
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-23 23:31:07 +0530

    working on option tab file

--- src/components/OptionsTab.js
+++ src/components/OptionsTab.js
@@ -4,7 +4,41 @@ import React from "react";
 import { locationsTab } from "data/Mock-data";
 
 const OptionsTab = () => {
-  return <Box></Box>;
+  const [value, setvalue] = useState(0);
+
+  const handleChange = (e, newValue) => {
+    setvalue(newValue);
+  };
+  return (
+    <Container maxWidth="xl">
+      <Box
+        sx={{
+          display: "flex",
+          flexGrow: 1,
+          px: { xs: 0, md: 2 },
+          alignItems: "center",
+          mt: 2,
+          mb: 2,
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons
+          sx={{
+            [`& .${tabsClasses.scrollButtons}`]: {
+              "&.Mui-disabled": { opacity: 0.3 },
+            },
+          }}
+        >
+            {
+                
+            }
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default OptionsTab;
