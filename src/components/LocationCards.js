b5da789106837d4655cc3f04fc38aeffb62ba886
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-25 23:35:27 +0530

    started location cards file

--- src/components/LocationCards.js
+++ src/components/LocationCards.js
@@ -1,15 +1,27 @@
-import { Box, Grid } from '@mui/material'
-import { locations as CardLocations } from 'data/Mock-data'
-import React, { useState } from 'react'
-
+import { Box, Grid } from "@mui/material";
+import { locations as CardLocations } from "data/Mock-data";
+import React, { useState } from "react";
 
 const LocationCards = () => {
-    const [cards, ] te(second)
+  const [cards] = useState(CardLocations);
+  if (!cards.length) {
+    return null;
+  }
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box sx={{ mx: 2 }}>
+      <Grid container rowSpacing={3} columnSpacing={3}>
+        {
+            cards.map(location=>{
+                return (
+                    <Grid key={location.id} xs={12} sm={2} md={4} lg={3} >
 
-export default LocationCards
+                    </Grid>
+                )
+            })
+        }
+      </Grid>
+    </Box>
+  );
+};
+
+export default LocationCards;
