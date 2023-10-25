10aede41914e9231bc21ed3175a07badb4dd2b95
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-25 23:29:49 +0530

    started location cards file

---
+++ src/components/LocationCards.js
@@ -0,0 +1,15 @@
+import { Box, Grid } from '@mui/material'
+import { locations as CardLocations } from 'data/Mock-data'
+import React, { useState } from 'react'
+
+
+const LocationCards = () => {
+    const [cards, ] te(second)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LocationCards
