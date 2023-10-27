458ad0d1371a1953b851bcd4c5d4ea4ff08e2771
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-27 22:52:22 +0530

    working on swipeable card ui

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -1,32 +1,36 @@
-import { Box, Button, MobileStepper, Typography } from '@mui/material'
-import React, { useState } from 'react'
-import SwipeableViews from 'react-swipeable-views-utils'
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { Box, Button, MobileStepper, Typography } from "@mui/material";
+import React, { useState } from "react";
+import SwipeableViews from "react-swipeable-views-utils";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { AiFillStar } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
-import { carouselDot, carouselImage, dFlex, fixedBottom, fixedIcon, flexBetween } from 'themes/CommonTheme';
+import {
+  carouselDot,
+  carouselImage,
+  dFlex,
+  fixedBottom,
+  fixedIcon,
+  flexBetween,
+} from "themes/CommonTheme";
 
-
-
-const CarouselCard = ({location}) => {
-    const [activestep, setActivestep] = useState();
-    const maxStep = location.locationImages.length;
-    const handleNext = ()=>{
-        setActivestep((prevActiveStep)=>prevActiveStep+1)
-    }
-    const handleBack = ()=>{
-        setActivestep((prevActiveStep)=>prevActiveStep-1)
-
-    }
-    const handleStepChange = (step)=>{
-setActivestep(step)
-    }
+const CarouselCard = ({ location }) => {
+  const [activestep, setActivestep] = useState();
+  const maxStep = location.locationImages.length;
+  const handleNext = () => {
+    setActivestep((prevActiveStep) => prevActiveStep + 1);
+  };
+  const handleBack = () => {
+    setActivestep((prevActiveStep) => prevActiveStep - 1);
+  };
+  const handleStepChange = (step) => {
+    setActivestep(step);
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box className="carouselCards">
 
-export default CarouselCard
+    </Box>
+  );
+};
+
+export default CarouselCard;
