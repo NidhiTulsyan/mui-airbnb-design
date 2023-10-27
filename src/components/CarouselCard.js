a37d73eb9cd2ca3a5c28874be8fc6e843e901e5d
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-27 22:42:48 +0530

    starting caroiusel card .js

--- src/components/CarouselCard.js
+++ src/components/CarouselCard.js
@@ -1,5 +1,10 @@
 import { Box, Button, MobileStepper, Typography } from '@mui/material'
 import React from 'react'
+import swipeableviews from 'react-swipeable-views-utils'
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { AiFillStar } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
 
 const CarouselCard = () => {
   return (
