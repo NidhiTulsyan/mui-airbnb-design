5f4ead40b070e3b0631d0ea9eab1117bd9ac37dd
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-27 22:39:25 +0530

    starting caroiusel card .js

---
+++ src/components/CarouselCard.js
@@ -0,0 +1,12 @@
+import { Box, Button, MobileStepper, Typography } from '@mui/material'
+import React from 'react'
+
+const CarouselCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CarouselCard
