3d32b7fe71438a9c0ab2ddf4ceab9ef668017f36
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-04 23:51:35 +0530

    added fre requiste imports in footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import { Box, Button, Container, Link, Paper, Stack } from '@mui/material'
 import React from 'react'
-import { flexBetweenCenter } from 'themes/CommonTheme'
+import { flexBetweenCenter , justifyCenter,fullWidthFlex } from 'themes/CommonTheme'
+import { BsGlobe } from "react-icons/bs";
+import { IoChevronUpOutline } from "react-icons/io5";
+
 
 
 const Footer = () => {
