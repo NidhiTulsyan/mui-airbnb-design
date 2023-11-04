eaa4684b10a5557b64427f4bc5d269bb59dab3e8
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-04 23:52:44 +0530

    added indentation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,15 @@
-import { Box, Button, Container, Link, Paper, Stack } from '@mui/material'
-import React from 'react'
-import { flexBetweenCenter , justifyCenter,fullWidthFlex } from 'themes/CommonTheme'
+import { Box, Button, Container, Link, Paper, Stack } from "@mui/material";
+import React from "react";
+import {
+  flexBetweenCenter,
+  justifyCenter,
+  fullWidthFlex,
+} from "themes/CommonTheme";
 import { BsGlobe } from "react-icons/bs";
 import { IoChevronUpOutline } from "react-icons/io5";
 
-
-
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  ) 
-}
+  return <div></div>;
+};
 
-export default Footer
+export default Footer;
