4d0db537d7141482697da60dfb20c4f44fa20484
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-04 23:49:07 +0530

    working on footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
+import { Box, Link, Paper, Stack } from '@mui/material'
 import React from 'react'
+<Box
 
 const Footer = () => {
   return (
     <div>
       
     </div>
-  )
+  ) 
 }
 
 export default Footer
