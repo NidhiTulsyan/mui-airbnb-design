da818c052b1a860e204128173198cc2e129f16d9
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-05 23:12:51 +0530

    adding footerlink

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,15 @@ import {
 import { BsGlobe } from "react-icons/bs";
 import { IoChevronUpOutline } from "react-icons/io5";
 
-const FooterLinks={
-  id:1,text:"",url:"",
-  id:2,text:"",url:"",
-  id:3,text:"",url:"",
-  id:4,text:"",url:""
-}
+const FooterLinks = [
+  { id: 1, text: "Privacy", url: "#" },
+  { id: 2, text: "Terms", url: "#" },
+  { id: 3, text: "SiteMap", url: "#" },
+  { id: 4, text: "Destinations", url: "#" },
+];
 
 const Footer = () => {
-  return <div></div>;
+  return <Box></Box>;
 };
 
 export default Footer;
