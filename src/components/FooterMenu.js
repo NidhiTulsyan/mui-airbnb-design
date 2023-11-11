49640db9b8fa51f3de0ebe7c3f2fcf6863138272
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-11 23:52:28 +0530

    adding footermenu const

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,18 +1,14 @@
-import { Box, Button, Stack, Typography } from '@mui/material'
-import React from 'react'
-import { FaRegUserCircle,FaSearch,FaRegHeart } from "react-icons/fa";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import { FaRegUserCircle, FaSearch, FaRegHeart } from "react-icons/fa";
 
 const footerMenu = [
-  {id:1,text:"",icon:<FaSearch />},
-  {id:2,text:"",icon:<FaRegHeart />},
-  {id:3,text:"",icon:<FaRegUserCircle />},
-]
+  { id: 1, text: "Explore", icon: <FaSearch /> },
+  { id: 2, text: "Wishlist", icon: <FaRegHeart /> },
+  { id: 3, text: "Login", icon: <FaRegUserCircle /> },
+];
 const FooterMenu = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default FooterMenu
+export default FooterMenu;
