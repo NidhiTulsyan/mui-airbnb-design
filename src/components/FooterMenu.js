11e8316820f51ce4b83742f942285eb579ff448d
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-11 23:49:22 +0530

    adding few material ui components

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,5 +1,7 @@
 import { Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { FaRegUserCircle,FaSearch,FaRegHeart } from "react-icons/fa";
+
 
 const FooterMenu = () => {
   return (
