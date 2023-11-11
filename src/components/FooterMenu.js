bb4ab921496f7c4161f1bd3a40865086d2288140
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-11 23:51:32 +0530

    adding footermenu const

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -2,7 +2,11 @@ import { Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
 import { FaRegUserCircle,FaSearch,FaRegHeart } from "react-icons/fa";
 
-
+const footerMenu = [
+  {id:1,text:"",icon:<FaSearch />},
+  {id:2,text:"",icon:<FaRegHeart />},
+  {id:3,text:"",icon:<FaRegUserCircle />},
+]
 const FooterMenu = () => {
   return (
     <div>
