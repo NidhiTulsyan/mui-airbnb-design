4cf737f928d9f7aa4b3e707e1ee8c439cad1afd8
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-11-11 23:48:29 +0530

    adding few material ui components

--- src/components/FooterMenu.js
+++ src/components/FooterMenu.js
@@ -1,10 +1,10 @@
-import { Button, Stack } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const FooterMenu = () => {
   return (
     <div>
-      <Button></Button>
+      
     </div>
   )
 }
